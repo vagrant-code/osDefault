78b252766a42ab4d686afe86127bff590150f6d8
Ivan Kozhukhov <cheremhovo1990@yandex.ru>
2017-07-11 10:24:11 +0800

    init

---
+++ vagrant/provision/once-as-root.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#== Import script args ==
+
+timezone=$(echo "$1")
+
+#== Bash helpers ==
+
+function info {
+  echo " "
+  echo "--> $1"
+  echo " "
+}
+
+info "Configure timezone"
+timedatectl set-timezone ${timezone} --no-ask-password
